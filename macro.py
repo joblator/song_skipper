b859124a5eeb7eba189fb1e26c2902e774d42ab5
joblator <janjoel.bonisch@gmail.com>
2025-10-31 22:43:01 +0200

    updated it with spotify hotkeys and made more functions

--- macro.py
+++ macro.py
@@ -1,22 +1,50 @@
 import pyautogui
 import keyboard
 import time
-screen_width, screen_height = pyautogui.size()
-print(screen_width,screen_height)
-screen_width /= 1.9 - (0.03 *((2560 - screen_width) // 640))
-screen_height /= 1.1 + (0.03 * ((1440 - screen_height) // 360))
+def switch_app():
+    keyboard.press_and_release('alt+tab')
+    time.sleep(0.3)
 def Skip_song():
-    keyboard.press_and_release('alt + tab')
+    while keyboard.is_pressed('`ctrl') or keyboard.is_pressed('left'):
+        time.sleep(0.01)
+    switch_app()
     time.sleep(0.3)
-    pyautogui.moveTo(screen_width, screen_height)
-    pyautogui.click()
+    keyboard.press_and_release('ctrl+right')
     time.sleep(0.3)
-    keyboard.press_and_release('alt + tab')
+    switch_app()
+
+
+
+# if the song is at the very start it will go to the previous song
+def go_to_current_song_start():
+    switch_app()
+    keyboard.press_and_release('ctrl+left')
+    time.sleep(0.3)
+    switch_app()
+    
+def previous_song():
+    while keyboard.is_pressed('ctrl') or keyboard.is_pressed('right'):
+        time.sleep(0.01)
+    switch_app()
+    time.sleep(0.3)
+    keyboard.press_and_release('ctrl+left')
+    time.sleep(0.3)
+    keyboard.press_and_release('ctrl+left')
+    time.sleep(0.3)
+    switch_app()
+    
 def stop():
     print("program stopped")
     quit()
-print("starting Program press ` to skip songs and ~ to stop the program")
-keyboard.add_hotkey("`",Skip_song)
+print("Song Control Program Started")
+print("Controls:")
+print("  Ctrl + Left =  Skip song")
+print("  Ctrl + Right  =  Previous song")
+print("  ` (backtick) =  Restart current song")
+print("  F1 = Stop program\n")
+keyboard.add_hotkey('ctrl+left', Skip_song)
 keyboard.add_hotkey("f1",stop)
+keyboard.add_hotkey("ctrl+right",previous_song)
+keyboard.add_hotkey("`",go_to_current_song_start)
 keyboard.wait()
 print("the program finshed")
