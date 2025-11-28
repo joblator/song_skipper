bbfae774e84758d2165c4cc8e8f3c87b1b3be3f0
joblator <janjoel.bonisch@gmail.com>
2025-11-28 18:09:07 +0200

    made the code work with media keys

--- macro.py
+++ macro.py
@@ -1,51 +1,62 @@
-import keyboard
 import time
+import keyboard
+from pynput.keyboard import Controller, Key
 
+DEBUG = False
+media_keyboard = Controller()
 
-def switch_app():
-    keyboard.press_and_release('alt+tab')
-    time.sleep(0.3)
-def Skip_song():
-    while keyboard.is_pressed('ctrl') or keyboard.is_pressed('left'):
-        time.sleep(0.01)
-    switch_app()
-    time.sleep(0.3)
-    keyboard.press_and_release('ctrl+right')
-    time.sleep(0.3)
-    switch_app()
-
-
-
-# if the song is at the very start it will go to the previous song
-def go_to_current_song_start():
-    switch_app()
-    keyboard.press_and_release('ctrl+left')
-    time.sleep(0.3)
-    switch_app()
-    
-def previous_song():
+def skip_song():
+    if DEBUG:
+        print("[ENTER] skip_song")
     while keyboard.is_pressed('ctrl') or keyboard.is_pressed('right'):
         time.sleep(0.01)
-    switch_app()
-    time.sleep(0.3)
-    keyboard.press_and_release('ctrl+left')
-    time.sleep(0.3)
-    keyboard.press_and_release('ctrl+left')
-    time.sleep(0.3)
-    switch_app()
-    
-def stop():
-    print("program stopped")
+    media_keyboard.press(Key.media_next)
+    media_keyboard.release(Key.media_next)
+    if DEBUG:
+        print("[EXIT] skip_song")
+
+def previous_song():
+    if DEBUG:
+        print("[ENTER] previous_song")
+    while keyboard.is_pressed('ctrl') or keyboard.is_pressed('left'):
+        time.sleep(0.01)
+    media_keyboard.press(Key.media_previous)
+    media_keyboard.release(Key.media_previous)
+    if DEBUG:
+        print("[EXIT] previous_song")
+
+def go_to_current_song_start():
+    if DEBUG:
+        print("[ENTER] go_to_current_song_start")
+    while keyboard.is_pressed('`'):
+        time.sleep(0.01)
+    media_keyboard.press(Key.media_previous)
+    media_keyboard.release(Key.media_previous)
+    time.sleep(0.1)
+    media_keyboard.press(Key.media_next)
+    media_keyboard.release(Key.media_next)
+    if DEBUG:
+        print("[EXIT] go_to_current_song_start")
+
+def stop_program():
+    if DEBUG:
+        print("[ENTER] stop_program")
+    print("Program stopped")
+    if DEBUG:
+        print("[EXIT] stop_program")
     quit()
+
 print("Song Control Program Started")
 print("Controls:")
-print("  Ctrl + Left =  Skip song")
-print("  Ctrl + Right  =  Previous song")
-print("  ` (backtick) =  Restart current song")
+print("  Ctrl + Right = Skip song")
+print("  Ctrl + Left  = Previous song")
+print("  ` (backtick) = Restart current song")
 print("  F1 = Stop program\n")
-keyboard.add_hotkey('ctrl+left', Skip_song)
-keyboard.add_hotkey("f1",stop)
-keyboard.add_hotkey("ctrl+right",previous_song)
-keyboard.add_hotkey("`",go_to_current_song_start)
+
+keyboard.add_hotkey('ctrl+right', skip_song)
+keyboard.add_hotkey('ctrl+left', previous_song)
+keyboard.add_hotkey('`', go_to_current_song_start)
+keyboard.add_hotkey('f1', stop_program)
+
 keyboard.wait()
-print("the program finshed")
+print("The program finished")
